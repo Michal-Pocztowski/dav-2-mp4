0927842f4b2cc18778ad93e54ca85b1560812637
Michal <michal.pocztowski@skygate.io>
2022-01-08 13:43:26 +0100

    Add video renaming option

--- main.py
+++ main.py
@@ -26,24 +26,52 @@ def convert_videos(
         clamp=True,
         help="Width (pixels) of output videos",
     ),
+    rename_videos: bool = typer.Option(
+        False,
+        "--rename",
+        "-r",
+        help="Rename output videos name (base on parent folder name) or not.",
+    ),
 ):
-    for dav_video_path in dav_videos_path.glob("*.dav"):
-        video_name = dav_video_path.stem
-        subprocess.call(
-            [
-                "ffmpeg",
-                "-y",
-                "-i",
-                dav_video_path,
-                "-vf",
-                f"scale={output_video_width}:{output_video_height}",
-                "-vcodec",
-                "h264",
-                mp4_videos_path / f"{video_name}.mp4",
-                "-loglevel",
-                "error",
-            ]
-        )
+    if rename_videos:
+        # TODO: replace dav_videos_path.glob("*") with regex
+        for camera_folder_path in dav_videos_path.glob("*"):
+            camera_name = camera_folder_path.stem
+            for dav_video_path in camera_folder_path.glob("**/*.dav"):
+                video_name = dav_video_path.stem[13:]
+                subprocess.call(
+                    [
+                        "ffmpeg",
+                        "-y",
+                        "-i",
+                        dav_video_path,
+                        "-vf",
+                        f"scale={output_video_width}:{output_video_height}",
+                        "-vcodec",
+                        "h264",
+                        mp4_videos_path / f"{camera_name}_{video_name}.mp4",
+                        "-loglevel",
+                        "error",
+                    ]
+                )
+    else:
+        for dav_video_path in dav_videos_path.glob("**/*.dav"):
+            video_name = dav_video_path.stem
+            subprocess.call(
+                [
+                    "ffmpeg",
+                    "-y",
+                    "-i",
+                    dav_video_path,
+                    "-vf",
+                    f"scale={output_video_width}:{output_video_height}",
+                    "-vcodec",
+                    "h264",
+                    mp4_videos_path / f"{video_name}.mp4",
+                    "-loglevel",
+                    "error",
+                ]
+            )
 
 
 if __name__ == "__main__":
